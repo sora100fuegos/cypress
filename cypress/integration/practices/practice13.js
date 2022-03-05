38323081125412a4d39c2145602c29187b8e141b
carlos Cienfuegos <carlos.cienfuegos@globant.com>
2022-03-04 22:49:13 -0600

    practice 13 added

--- cypress/integration/practices/practice13.js
+++ cypress/integration/practices/practice13.js
@@ -1,30 +1,27 @@
+/*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click 'View Product' for any product on home page
+5. Verify product detail is opened
+6. Increase quantity to 4
+7. Click 'Add to cart' button
+8. Click 'View Cart' button
+9. Verify that product is displayed in cart page with exact quantity
+*/
 const url = Cypress.env('base-url');
 
  it ('verify product quantity in card', () => {
 cy.visit(url);
 cy.url().should('contains', url);
-cy.url.should('contains', url);
 
-cy.get('a[href="/products"]').click()
-cy.url.should('contains', `${url}/products`);
-//cy.get('.overlay-content').trigger('mouseover')
-cy.get('.overlay-content>[data-product-id="1"]').click({multiple: true, force: true} )
-cy.get('.btn-success').click();
-cy.get('.overlay-content>a[data-product-id="2"]').click({ multiple: true , force: true} )
+cy.get('a[href="/product_details/1"]').click()
+cy.url().should('contains', `${url}product_details/1`);
+cy.get('#quantity').clear()
+cy.get('#quantity').type('4')
+cy.get('.cart').click();
 cy.get('.btn-success').click();
 cy.get('a[href="/view_cart"]').first().click()
-cy.get('#product-1').should('be.visible')
-cy.get('#product-1>td.cart_price').should('be.visible')
-cy.get('#product-1>td.cart_quantity').should('be.visible')
-cy.get('#product-1>td.cart_total').should('be.visible')
-
-cy.get('#product-2').should('be.visible')
-cy.get('#product-2>td.cart_price').should('be.visible')
-cy.get('#product-2>td.cart_quantity').should('be.visible')
-cy.get('#product-2>td.cart_total').should('be.visible')
-
-
-//cy.get('.btn-success').click();
-//cy.url().should('contains', 'https://automationexercise.com/view_cart');
+cy.get('#product-1 .disabled').should('have.text', '4')
 
  });
